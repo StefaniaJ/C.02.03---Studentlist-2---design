6627125e7067b06672653dd958a52ba862cb3be8
Stefania <44357465+StefaniaJ@users.noreply.github.com>
2019-09-03 20:43:34 +0200

    added some coments

---
+++ script.js
@@ -0,0 +1,24 @@
+//Declare all the variables
+document.addEventListener("DOMContentLoaded", start);
+//Add the template, main, link, modal..
+
+//Call function start
+function start() {
+  console.log("Run function start");
+}
+
+//Create a varible for first name -split the fullname
+
+//Create a variable for last name - split the fullname
+
+//Load the data from JSON
+
+// Display the student list using a "show" function
+
+//Select the content from template using a clone
+//Change the content from template
+//Apend the clone to the main
+
+//load the data
+
+//create a filter for first name, last name, house
